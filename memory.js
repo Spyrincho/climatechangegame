f8f60caef4c70ce95e0e2a0e2255bc95a525ac94
Patrick Homburg <42439682+Spyrincho@users.noreply.github.com>
2019-02-14 10:16:35 +0100

    Create memory.js

---
+++ memory.js
@@ -0,0 +1,77 @@
+//Score
+let hitxt = document.getElementById('hit');
+let misstxt = document.getElementById('miss');
+let txt1 = "";
+let txt2 = "";
+let hit = 0;
+let miss = 0;
+// ^^ SCORE
+
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  //HITS
+  hit++;
+  txt1 = hit;
+  hitxt.innerHTML = "Hits: " + txt1;
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  //MISSES
+  miss++;
+  txt2 = miss;
+  misstxt.innerHTML = "Misses: " + txt2;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
